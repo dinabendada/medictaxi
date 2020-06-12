944d38d1d72019c243349eb0c02d33a76731f957
maxstar538 <eljifree10@icloud.com>
2020-06-12 20:11:13 +0200

    fix

--- environment/production.js
+++ environment/production.js
@@ -1,8 +1,8 @@
 
 module.exports = {
     dbUrl: 'mongodb+srv://max:free@cluster0-p1f9v.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    cert: '',
-    key: '',
+    cert: '/etc/letsencrypt/live/www.medictaxi.fr/fullchain.pem',
+    key:  '/etc/letsencrypt/live/www.medictaxi.fr/privkey.pem',
     portHttp: 80,
     portHttps: 443
   }
